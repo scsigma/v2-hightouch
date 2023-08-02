6e6146a5e7aadaf4e17c1a4128420b6faf8d1d21
scsigma <sully@sigmacomputing.com>
2023-08-02 15:02:39 -0700

    working api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,55 @@
 import React, {useEffect} from 'react';
 
-function App() {
-  useEffect(() => {
-    fetch('/test')
+const App = () => {
+
+  const tester = () => {
+    const data = fetch('/test')
       .then((res) => res.json())
       .then((data) => console.log(data))
+
+    return data
+  }
+
+  const trigger_sync = () => {
+
+    const syncId = '1644239';
+    const apiToken = '98817d9d-cd97-452c-be64-b76bbd48ddaa'
+
+    fetch(`https://api.hightouch.com/api/v1/syncs/${syncId}/trigger`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiToken}`
+      }
+    })
+  }
+
+
+  useEffect(() => {
+    tester();
   }, [])
+  
+  
   return (
     <div>
       Hello World
+      <button
+      onClick={() => {
+        console.log('clicked!')
+        fetch('/hightouch_sync', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            "token": "i am a token",
+            "syncId": "i am a sync id"
+          })
+        })
+        trigger_sync()
+        return tester()
+      }}
+      >Click Me</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const port = 3001;
 
 const app = express();
+app.use(express.json());
 
 app.get('/test', (req, res) => {
     res.send({ data: "Hello from the server!"})
+})
+
+app.post('/hightouch_sync', (req, res) => {
+    const {token, syncId} = req.body;
+    res.json({ token, syncId})
+    // fetch('https://api.hightouch.com/api/v1/syncs/1644239/trigger')
 })
 
 app.listen(port, () => {
