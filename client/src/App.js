92b5628ad4ea6643116ae519f56725c93cd0793a
scsigma <sully@sigmacomputing.com>
2023-08-23 08:20:27 -0700

    cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,30 @@ import { ChakraProvider, Button, Cente, Box, Flex } from '@chakra-ui/react';
 
 // ---- Sigma Config -----
 client.config.configureEditorPanel([
-  { name: "source", type: "element"},
+  // { name: "source", type: "element"},
   { name: "Hightouch API Token", type: "text"},
   { name: "List Creator syncId", type: "text"},
-  { name: "Contact List Update syncId", type: "text"}
+  { name: "Contact List Update syncId", type: "text"},
+  { name: "Button Text", type: "text", defaultValue: "Export to HubSpot"}
 ]);
 // -----------------------
 
 const allSigmaDataReceived = (config) => {
-  if (config['Hightouch API Token'] == "" || config['List Creator syncId'] == "" || config['Contact List Update syncId'] == "") {
-    return false; 
-  }
-
-  return true;
+  return config['Hightouch API Token'] && config['List Creator syncId'] && config['Contact List Update syncId'];
 }
 
 const App = () => {
 
-  let [apiToken, setApiToken] = useState(null);
-  let [listCreatorSyncId, setListCreatorSyncId] = useState(null);
-  let [contactSyncId, setContactSyncId] = useState(null);
-  let [allData, setAllData] = useState(false);
+  const [apiToken, setApiToken] = useState(null);
+  const [listCreatorSyncId, setListCreatorSyncId] = useState(null);
+  const [contactSyncId, setContactSyncId] = useState(null);
+  const [allData, setAllData] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
   // sigma stuff
   const config = useConfig();
   if (!allSigmaDataReceived(config)) {
-    // this means that data is missing
+    // this means that data is missing from the config panel in Sigma
     console.log('DATA IS MISSING')
   }
 
@@ -44,7 +43,7 @@ const App = () => {
 
 
 
-  const trigger_sync = async (listCreatorSyncId, contactSyncId, apiToken) => {
+  const triggerSync = async (listCreatorSyncId, contactSyncId, apiToken) => {
     // create or update the new campaign list
     await fetch(`https://api.hightouch.com/api/v1/syncs/${listCreatorSyncId}/trigger`, {
       method: 'POST',
@@ -64,6 +63,15 @@ const App = () => {
     })
   }
 
+  const handleClick = () => {
+    setButtonClicked(true);
+
+    // set the button clicked back to false so the icon goes away
+    setTimeout(() => {
+      setButtonClicked(false);
+    }, 5000); // 5000ms = 5s
+  }
+
   return (
     <ChakraProvider>
       <Box justifyContent="center" display="flex" alignItems="center" paddingTop="9px">
@@ -71,18 +79,17 @@ const App = () => {
         backgroundColor="#ff5c35"
         color="white"
         _hover={{ backgroundColor: "#fff4f2", color: "#ff5c35"}}
+        style={{ width: '200px'}}
         onClick={() => {
           if (allSigmaDataReceived) {
-            console.log('clicked!')
-            console.log('apiToken', apiToken)
-            console.log('List Creator syncId', listCreatorSyncId)
-            console.log('Contact List syncId', contactSyncId)
-            trigger_sync(listCreatorSyncId, contactSyncId, apiToken)
-          } else {
-            console.log('click not working')
-          }
+            triggerSync(listCreatorSyncId, contactSyncId, apiToken)
+            handleClick()
+          } 
         }}
         >Export to Hubspot</Button>
+        {buttonClicked && 
+          <div style={{position: 'absolute', width: '50px', left: '76%'}}>✅</div>
+        }
       </Box>
     </ChakraProvider>
   );
